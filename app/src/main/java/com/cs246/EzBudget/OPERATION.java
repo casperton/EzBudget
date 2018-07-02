84e5c38ec1ae4cd4a5a3c85a4aff72f75ff32838
rasputtim <rasputtim@gmx.com>
2018-07-02 00:13:46 -0300

    added some observations to the code

--- app/src/main/java/com/cs246/EzBudget/OPERATION.java
+++ app/src/main/java/com/cs246/EzBudget/OPERATION.java
@@ -11,9 +11,9 @@ public class OPERATION {
   // Fields
   //
   public static final Integer UNKNOWN = -1;
-  public static final Integer CREDIT = 0;
-  public static final Integer DEBIT = 1;
-  public static final Integer INFORMATIVE = 2;
+  public static final Integer CREDIT = 0; //indicates an Income
+  public static final Integer DEBIT = 1; //indicates an Expense
+  public static final Integer INFORMATIVE = 2; //indicates only informational value. not used to add or subtract.
   
   //
   // Constructors
