4103ca4bb0243c20ab4612d739a118e91a4a26c0
rasputtim <rasputtim@gmx.com>
2018-07-04 19:58:13 -0300

    added Tab to separate select View and Summary page. The summary was mmoved from the mainActivity to the SummaryFragment

--- app/src/main/java/com/cs246/EzBudget/mFragments/SelectViewFragment.java
+++ app/src/main/java/com/cs246/EzBudget/mFragments/SelectViewFragment.java
@@ -2,18 +2,39 @@ package com.cs246.EzBudget.mFragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.cs246.EzBudget.R;
+import com.cs246.EzBudget.mBackGrounds.BackGroundBalView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SelectViewFragment extends Fragment {
 
+
+
+    private RecyclerView myRecyclerView;
+    RecyclerView.LayoutManager myLayoutManager;
+    private View myView;
+    private FragmentManager myFagmentManager;
+    private ProgressBar myProgress=null;
+
+
+    @NonNull
+    static public SelectViewFragment newInstance(){
+        return new SelectViewFragment();
+    }
 
     public SelectViewFragment() {
         // Required empty public constructor
@@ -24,7 +45,24 @@ public class SelectViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_view, container, false);
+        myView =  inflater.inflate(R.layout.fragment_select_view, container, false);
+        super.onCreate(savedInstanceState);
+
+
+        myRecyclerView = (RecyclerView) myView.findViewById(R.id.listViewRecicler);
+        myLayoutManager = new LinearLayoutManager(getActivity());
+        myRecyclerView.setLayoutManager(myLayoutManager);
+        myRecyclerView.setHasFixedSize(true);
+
+        myProgress = (ProgressBar) myView.findViewById(R.id.listViewBar);
+        myProgress.setVisibility(View.INVISIBLE);
+        myFagmentManager = getActivity().getSupportFragmentManager();
+
+
+
+        new BackGroundBalView(myRecyclerView,myProgress,getActivity(),myFagmentManager ).execute();
+
+        return myView;
     }
 
 }
