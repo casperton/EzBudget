3d15950976dd9f48603e56f8215a4014c505e0b9
casperton <kirkchabries@gmail.com>
2018-07-14 12:57:09 -0500

    Add calculations needed to balance budget
    Add calculations to balance budget. Store any overages and carry them to previous pay periods when needed. Add a field to the summary layout to display the overag es.

--- app/src/main/java/com/cs246/EzBudget/Calculations.java
+++ app/src/main/java/com/cs246/EzBudget/Calculations.java
@@ -3,8 +3,8 @@ package com.cs246.EzBudget;
 import com.cs246.EzBudget.SummaryView.SummaryItem;
 import com.cs246.EzBudget.mFragments.SummaryFragment;
 
-import java.util.Collections;
 import java.util.List;
+import static java.lang.Math.abs;
 
 /**
  * <p>
@@ -17,7 +17,7 @@ public class Calculations {
 
 
     private double periodTotalNeeded = 0;
-    private double periodPaidTotal = 0;
+    private double periodUnPaidTotal = 0;
     private List<SummaryItem> list;
 
     /**
@@ -37,35 +37,40 @@ public class Calculations {
         for (int i = start_position + 1; i < list.size() && list.get(i).getType() != OPERATION.CREDIT; i++)  {
             SummaryItem currentItem = list.get(i);
             if (currentItem.getType() == OPERATION.DEBIT) {
+                // Total for this pay period
                 periodTotalNeeded += currentItem.getAmount();
-                if (currentItem.isPaid()) periodPaidTotal += currentItem.getAmount();
+                // Track unpaid items for pay period
+                if (!currentItem.isPaid()) periodUnPaidTotal += currentItem.getAmount();
             }
         }
     }
 
+    /**
+     * This method will parse the list and update the totals
+     * for each period
+     */
     public void updateTotals() {
-        int incomeItemIndex = -1;
-        boolean adjustExpenses = false;
-        String name;
         for (SummaryItem item: list) {
             if (item.getType() == OPERATION.CREDIT) {
                 getPeriodTotal(list.indexOf(item));
-                item.setTotal_needed(periodTotalNeeded);
-                item.setTotal_paid(periodPaidTotal);
-                // Check to see if the total needed exceeds the amount available
-                if (periodTotalNeeded > item.getAmount()) {
-                    adjustExpenses = true;
-                }
+                item.setExpenseTotal(periodTotalNeeded);
+                item.setUnpaidTotal(periodUnPaidTotal);
+                item.setOverageTotal(0); // Reset this each time items are changed
                 periodTotalNeeded = 0;
-                periodPaidTotal = 0;
+                periodUnPaidTotal = 0;
             }
         }
 
-        if (adjustExpenses) {
-//            adjustPayments();
-        }
+        adjustPayments(); // Balance payments if there are overages
     }
 
+    /**
+     * This method finds the previous income item in the list
+     * and returns the index to be used for balancing the budget
+     * over multiple income periods
+     * @param startPosition Starting position index
+     * @return              The index of the last income item
+     */
     public int getLastIncomeItem(int startPosition) {
         for (int i = startPosition - 1; i >= 0; i--)  {
             SummaryItem currentItem = list.get(i);
@@ -76,34 +81,67 @@ public class Calculations {
         return -1;
     }
 
+//    public int getLastIncomeItemWithBalance(int startPosition, double balanceNeeded) {
+//        int index = -1;
+//        Date now = new Date();
+//        Calendar c = Calendar.getInstance();
+//        c.setTime(now);
+//        c.add(Calendar.MONTH, -1);
+//        Date oneMonthAgo = c.getTime();
+//        do {
+//            index = getLastIncomeItem(startPosition);
+//            if (index >= 0)
+//                if ((list.get(index).getAmount() - list.get(index).getTotalCombined()) > balanceNeeded
+//                        && list.get(index).getDate().after(oneMonthAgo)) {
+//                    return index;
+//                } else {
+//                startPosition = index;
+//            }
+//        } while (index >=0);
+//        return -1;
+//    }
+
+//    public int getMoveableItem(int startPosition, double balanceLimit) {
+//        for (int x = startPosition + 1; x < list.size() && list.get(x).getType() != OPERATION.CREDIT; x++) {
+//            if (list.get(x).getAmount() < balanceLimit) return x;
+//        }
+//        return -1;
+//    }
+//
+//    public void swapItems(int itemIndex, int endPosition) {
+//        if (itemIndex > 0 && endPosition >= 0) {
+//            for (int i = itemIndex; i > endPosition; i--) {
+//                Collections.swap(list, i, i-1);
+//            }
+//        }
+//    }
+
+    /**
+     * This method will determine if each pay period has enough
+     * money to cover the expenses. If it does not, the overage
+     * will be carried to the previous pay period.
+     */
     public void adjustPayments() {
-        boolean swapChange = false;
         // Start at the end of the list
-        for (int i = list.size() - 1; i >= 0; i--) {
-            int lastIncomePosition = -1;
-            // Check if the item is an income item and if the total needed exceeds the amount
-            if (list.get(i).getType() == OPERATION.CREDIT && list.get(i).getTotal_needed() > list.get(i).getAmount()) {
-                // Find the last income item
-                if (i > 0) lastIncomePosition = getLastIncomeItem(i);
-                if (lastIncomePosition > 0) {
-                    // Check to see if any items can be moved to this income item
-                    for(int x = lastIncomePosition + 1; x < list.size() && list.get(x).getType() != OPERATION.CREDIT; i++) {
-                        // Check if this item is less than the balance of the previous item
-                        if (list.get(x).getAmount() < (list.get(lastIncomePosition).getAmount() - list.get(lastIncomePosition).getTotal_needed())) {
-                            // Swap the item until it's in the position behind the last income item
-                            for (int y = x; y >= lastIncomePosition - 1; i--) {
-                                Collections.swap(list, y, x);
-                            }
-                        }
-                        swapChange = true;
+        for (int currentIncomeIndex = list.size() - 1; currentIncomeIndex >= 0; currentIncomeIndex--) {
+
+            // Check if the item is an income item
+            if (list.get(currentIncomeIndex).getType() == OPERATION.CREDIT) {
+                //  Check if the total needed exceeds the amount of the income item
+                double periodBalance = list.get(currentIncomeIndex).getAmount() - list.get(currentIncomeIndex).getTotalUnpaidCombined();
+
+                int lastIncomePosition = -1;
+                // If the periodBalance is < 0, there isn't enough to cover expenses
+                if (periodBalance < 0) {
+                    // Move the overage amount to the previous billing period to be covered
+                    double overageAmount = abs(periodBalance);
+                    lastIncomePosition = getLastIncomeItem(currentIncomeIndex);
+                    if (lastIncomePosition >= 0) {
+                        list.get(currentIncomeIndex).setUnpaidTotal(list.get(currentIncomeIndex).getUnpaidTotal() - overageAmount);
+                        list.get(lastIncomePosition).setOverageTotal(overageAmount);
                     }
                 }
             }
-        }
-
-        if (swapChange) {
-            updateTotals();
-            adjustPayments();
         }
     }
 }
